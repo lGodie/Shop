c759b821653850ad8ea90ea7650511492d126d9d
DIEGO ALEJANDRO ZAPATA VANEGAS <diegozapata176738@correo.itm.edu.co>
2019-02-16 22:07:18 -0500

    yes

--- Shop.Web/Controllers/HomeController.cs
+++ Shop.Web/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Shop.Web.Controllers
 {
     public class HomeController : Controller
     {
-        //comentario
+        //comentarioddd
         public IActionResult Index()
         {
             return View();
